dca7ae45e05ba60d651f5e04f54ebaa36449bb61
Firman47 <122626748+Firman47@users.noreply.github.com>
2024-11-14 16:35:19 +0700

    delete comand

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -5,7 +5,6 @@ export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
 
-    // Panggil signUp dan tunggu hasilnya
     const status = await signUp(data);
 
     if (status) {
